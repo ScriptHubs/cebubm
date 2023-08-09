128c9366b2becafee9b531aa482106d8ac0a2559
tsujintsujin <117808080+tsujintsujin@users.noreply.github.com>
2023-08-10 07:41:38 +0800

    title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Cebu Business Months</title>
+
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
